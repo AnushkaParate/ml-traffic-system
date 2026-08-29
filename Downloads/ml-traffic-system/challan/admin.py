22435ce5f096b04746a72561457dd129455b93b8
Aaryan <aaryansah690@gmail.com>
2026-08-30 01:21:55 +0530

    feat: admin registrations for challan/signal_control, formal split-screen Ui

--- Downloads/ml-traffic-system/challan/admin.py
+++ Downloads/ml-traffic-system/challan/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Challan, Violation
+
+
+@admin.register(Violation)
+class ViolationAdmin(admin.ModelAdmin):
+    list_display = ('violation_type', 'vehicle', 'confidence_score', 'detected_at')
+    list_filter = ('violation_type',)
+    search_fields = ('vehicle__plate_number', 'video_source')
+
+
+@admin.register(Challan)
+class ChallanAdmin(admin.ModelAdmin):
+    list_display = ('violation', 'fine_amount', 'status', 'email_sent', 'issued_at')
+    list_filter = ('status', 'email_sent')
--- Downloads/ml-traffic-system/signal_control/admin.py
+++ Downloads/ml-traffic-system/signal_control/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SignalLog
+
+
+@admin.register(SignalLog)
+class SignalLogAdmin(admin.ModelAdmin):
+    list_display = ('intersection_id', 'lane', 'vehicle_count', 'ambulance_detected', 'signal_state', 'timestamp')
+    list_filter = ('signal_state', 'ambulance_detected')
